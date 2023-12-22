cfd6b2ae9695300fab97afb50a4c9b1d48ef830a
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2023-12-22 07:41:36 -0300

    Add SideA.vue component to sidebar

---
+++ src/sidebar/SideA.vue
@@ -0,0 +1,30 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "SideBar",
+  props: {
+    title: {
+      type: String,
+      default: "Link",
+    },
+    image: {
+      type: String,
+      default: "",
+    },
+    url: {
+      type: String,
+      default: "",
+    },
+  },
+});
+</script>
+
+<template>
+  <li class="nav-item py-2 py-sm-0">
+    <a :href="url" class="nav-link">
+      <span class="fs-5"><img :src="image" class="img-fluid" /></span>
+      <span class="fs-4 ms-4 d-none d-sm-inline">{{ title }}</span>
+    </a>
+  </li>
+</template>
