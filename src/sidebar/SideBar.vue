38e7f4a514cea174456b5a2ed1c348b949c4b882
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2023-12-22 07:20:33 -0300

    Add SideBar component

---
+++ src/sidebar/SideBar.vue
@@ -0,0 +1,27 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "SideBar",
+  props: {
+    title: {
+      type: String,
+      default: "Vasak",
+    },
+  },
+});
+</script>
+
+<template>
+  <div
+    class="sidebar col-auto p-2 col-md-4 col-xl-3 col-xxl-2 d-flex flex-column justify-content-between"
+  >
+    <a
+      href="#"
+      class="d-flex text-decoration-none align-items-center text-center"
+    >
+      <span class="fs-4 d-none d-sm-inline">{{ title }}</span>
+    </a>
+    <slot />
+  </div>
+</template>
