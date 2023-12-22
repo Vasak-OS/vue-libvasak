555cb1e9711a7c8d7b03eb450e149b8af48794f3
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2023-12-22 07:41:42 -0300

    Update imports in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
-import WindowFrame from './window/WindowFrame.vue';
+import SideA from "./sidebar/SideA.vue";
+import SideBar from "./sidebar/SideBar.vue";
+import SideRouterLink from "./sidebar/SideRouterLink.vue";
+import SideSection from "./sidebar/SideSection.vue";
+import WindowFrame from "./window/WindowFrame.vue";
 
-export {
-    WindowFrame
-}
+export { SideA, SideBar, SideRouterLink, SideSection, WindowFrame };
