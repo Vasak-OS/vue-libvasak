4a80134c7749d490c794c6679100efe17cf74952
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2023-12-28 10:45:25 -0300

    Add SideButton component to sidebar

---
+++ src/sidebar/SideButton.vue
@@ -0,0 +1,26 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "SideButton",
+  props: {
+    title: {
+      type: String,
+      default: "Link",
+    },
+    image: {
+      type: String,
+      default: "",
+    }
+  },
+});
+</script>
+
+<template>
+  <li class="nav-item py-2 py-sm-0">
+    <a href="#" class="nav-link">
+      <span class="fs-5"><img :src="image" class="img-fluid" /></span>
+      <span class="fs-4 ms-4 d-none d-sm-inline">{{ title }}</span>
+    </a>
+  </li>
+</template>
